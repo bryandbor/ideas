dffe8664130b40add59774b1c1877311c26750e0
bryandbor <bryandbor@gmail.com>
2014-05-08 23:00:37 -0700

    Better formatting and Index Page Completion

--- js/indexJquery.js
+++ js/indexJquery.js
@@ -1,10 +1,10 @@
 $(".hoverable").mousemove( function(e) {
     
-    var top = e.clientY + 10;
-    var left = e.clientX + 10;
+    var top = e.clientY - 250;
+    var left = e.clientX - 520;
     
-    $('#hoverDiv').css('top', top).css('left', left).fadeIn('fast').html($(this).attr('hoverText'));
-    
+    $('#hoverDiv').css('top', top).css('left', left).fadeIn('fast').html($(this).attr('hovertext'));
+
 }).mouseout(function() {
     $('#hoverDiv').fadeOut('fast');
 });
