e6a5df04dac12a95ac9e8be27d8b3385c0365698
bryandbor <bryandbor@gmail.com>
2014-05-08 19:04:44 -0700

    Picture now fades in and out

--- js/indexJquery.js
+++ js/indexJquery.js
@@ -3,8 +3,8 @@ $(".hoverable").mousemove( function(e) {
     var top = e.clientY + 10;
     var left = e.clientX + 10;
     
-    $('#hoverDiv').css('top', top).css('left', left).show().html($(this).attr('hoverText'));
+    $('#hoverDiv').css('top', top).css('left', left).fadeIn('fast').html($(this).attr('hoverText'));
     
 }).mouseout(function() {
-    $('#hoverDiv').hide();
+    $('#hoverDiv').fadeOut('fast');
 });
