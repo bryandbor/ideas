2f2b901dc4aa7feb7c20b647329205ae89cfb081
bryandbor <bryandbor@gmail.com>
2014-05-09 00:15:04 -0700

    Projects page in progress

--- js/indexJquery.js
+++ js/indexJquery.js
@@ -1,10 +1,11 @@
 $(".hoverable").mousemove( function(e) {
     
-    var top = e.clientY - 250;
-    var left = e.clientX - 520;
+    var top = e.clientY - 200 ;
+    var left = e.clientX - 620;
     
     $('#hoverDiv').css('top', top).css('left', left).fadeIn('fast').html($(this).attr('hovertext'));
 
 }).mouseout(function() {
     $('#hoverDiv').fadeOut('fast');
 });
+
